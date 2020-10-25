a8ed4aa4858a7140872d99c4e058c458b84b5074
Chiraag Balu <chiraag0729@outlook.com>
2020-10-24 21:09:09 -0700

    Initial Commit

---
+++ RockPaperScissors/RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsApp.swift
+//  RockPaperScissors
+//
+//  Created by Chiraag Balu on 10/24/20.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
